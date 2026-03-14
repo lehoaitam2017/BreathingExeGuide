f3e2df704422b47bc7feb821e1002bd9466a57f2
tatale <tam.le@oracle.com>
2026-03-14 09:43:05 -0700

    Initial Commit

---
+++ BreathingExeGuide/BreathingExeGuideApp.swift
@@ -0,0 +1,32 @@
+//
+//  BreathingExeGuideApp.swift
+//  BreathingExeGuide
+//
+//  Created by Tam Le on 3/14/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BreathingExeGuideApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
